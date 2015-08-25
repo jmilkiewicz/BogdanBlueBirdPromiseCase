38e378eaab284568ad44ad534e703341f449eac9
jmilkiewicz <jmilkiewicz@gmail.com>
2015-08-25 20:26:53 +0200

    polishing info

--- test/bdd/someTest.js
+++ test/bdd/someTest.js
@@ -4,21 +4,20 @@ var chai = require('chai');
 var chaiAsPromised = require("chai-as-promised");
 var should = chai.should();
 var expect = chai.expect;
-// Uncomment chai.use(chaiAsPromised) to start see that tests which shall fail finally starts failing !
-// With chaiAsPromised not enabled,  we see that all tests pass (which is wrong)
+// Uncomment chai.use(chaiAsPromised) to see that tests which shall fail finally start failing !
+// With chaiAsPromised NOT enabled,  we see that all tests pass (which is wrong)
 // and we also got some nasty `Unhandled rejection Error: Bogdan` on console
 // Why is all that? The answer seems to be pretty simple:
 // if chaiAsPromised is NOT enable than to.be.fulfilled / to.be.rejected simply returns 'undefined' instead of promise.
 // Undefined is a 'correct' value for Mocha so it does not mark the test as failed
 // (so all tests are green albeit "shall fail" tests shall be red).
 // Fortunately bluebird (probably within application shutdown hook) will simply print out a single nasty Unhandled rejection Error
-// when for every REJECTED promise (in our case 6 times (3 X rejected Bogdan and 3 times rejected Kuba )) !
-// according to comment of petkaantonov on https://github.com/petkaantonov/bluebird/issues/493 this in intended behaviour.
-    
-// To check it simply run ONLY "resolved promise" context
-// (with chai.use(chaiAsPromised) commented out) and you shall see that all test pass (which is wrong) but without any
+// when for every REJECTED promise (in our case 6 times (3 X rejected Bogdan and 3 times rejected Kuba ))
+// To experience that simply run ONLY "resolved promise" context
+// (with chai.use(chaiAsPromised) commented out) and you shall see that all test pass (which is wrong) without any
 // Unhandled rejection Error on console. 'Unhandled rejection Error' is simply a warning from bluebird that your application
 // is about to shutdown but you still have "rejected promises" which are not handled.
+// according to comment of petkaantonov on https://github.com/petkaantonov/bluebird/issues/493 this is an intended behaviour.
 
 // As soon as you have chaiAsPromised enabled than:
 // all "shall fail" tests  are finally red,
